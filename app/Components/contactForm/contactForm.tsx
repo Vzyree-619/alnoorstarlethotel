1ababa7796a4d6ab4fffb94ab660b85dad569b62
vzyree619 <vzyree619@gmail.com>
2024-07-31 05:54:07 +0500

    added sqlite and some form Validation settings

--- app/Components/contactForm/contactForm.tsx
+++ app/Components/contactForm/contactForm.tsx
@@ -1,25 +1,36 @@
 import styles from './contactForm.module.css';
 
 export default function ContactForm() {
+
+  async function contactSubmission(formData:any){
+    "use server"
+    const firstName =formData.get('firstN')
+    const secondName = formData.get('secondN')
+    const email = formData.get('email')
+    const contactNumber = formData.get('Number')
+    const Message = formData.get('Message')
+    console.log(firstName,secondName,email,contactNumber,Message)
+  }
+  
   return (
     <section className={styles.contactForm}>
       <div className={styles.contactForm}>
-        <form action=''>
+        <form action={contactSubmission}>
           <div className={styles.inputBox}>
             <label htmlFor=''>First Name</label>
-            <input type='text' />
+            <input type='text' name='firstN' />
           </div>
           <div className={styles.inputBox}>
             <label htmlFor=''>Last Name</label>
-            <input type='text' />
+            <input type='text' name='secondN' />
           </div>
           <div className={styles.inputBox}>
             <label htmlFor=''>Email</label>
-            <input type='email' />
+            <input type='email' name='email' />
           </div>
           <div className={styles.inputBox}>
             <label htmlFor=''>Phone Number</label>
-            <input type='number' />
+            <input type='number' name='Number' />
           </div>
           <div className={styles.select}>
             <h2>Select Subject</h2>
@@ -43,9 +54,9 @@ export default function ContactForm() {
             </div>
           </div>
           <div className={styles.textarea}>
-            <label htmlFor=''>Message</label>
+            <label className='' htmlFor=''>Message</label>
             <textarea
-              name=''
+              name='Message'
               id=''
               placeholder='Write your message...'
             ></textarea>
