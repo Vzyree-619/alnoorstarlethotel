7ffe6e019292180f8bc9e2cf73382bbec0eb0b7d
Inamullah619 <inamullahsixonenine@gmail.com>
2024-08-02 12:16:40 +0500

    Facvicon Add

---
+++ components/ui/imageUpload/nightForm.js
@@ -0,0 +1,51 @@
+import Link from 'next/link';
+import styles from '@/app/admin/dashboard/upload.module.css';
+import { ImageUp } from 'lucide-react';
+import Image from 'next/image';
+import DashSideBar from '@/app/admin/DashSideBar/DashSideBar';
+import DashHeader from '@/app/admin/DashHeader/DashHeader';
+import SubmitImage from './submitImage';
+import { useFormState } from 'react-dom';
+
+export default function NightImage(){
+    const [state, formAction] = useFormState(action, {});
+    return (
+        <section className={styles.dashboard}>
+          <DashHeader />
+          <div className={styles.dashboardContainer}>
+            <div className={styles.dashboardRow}>
+              <DashSideBar />
+              <div className={styles.dashboardColumn}>
+                <h1>Night</h1>
+                <div className={styles.uploadDiv}>
+                  <span>
+                    {/* <ImageUp /> */}
+                    <Image
+                      src={'/icon.png'}
+                      width={100}
+                      height={100}
+                      alt='Upload Image'
+                    />
+                  </span>
+                  <h2>Drag & drop to upload</h2>
+                </div>
+                {/* <button> */}
+                <form action={formAction}>
+                <label htmlFor='' className={styles.addImage}>
+                  <input type='file' name='ImageNight' id='' />
+                
+                </label>
+                
+                <SubmitImage/>
+            
+                </form>
+               
+                
+                {/* </button> */}
+                {/* <div className={styles.dashboardGrid}></div> */}
+              </div>
+            </div>
+          </div>
+        </section>
+      );
+}
