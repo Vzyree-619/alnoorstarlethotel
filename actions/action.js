b9f901ec462cfeb08e767b2ab4ce1edb1b8c19a3
Inamullah619 <inamullahsixonenine@gmail.com>
2024-08-03 11:01:30 +0500

    Action Conflig Solve

--- actions/action.js
+++ actions/action.js
@@ -1,112 +1,105 @@
-"use server"
-import { createMessage } from "@/lib/sendMessage"
-import { revalidatePath } from "next/cache"
-import { redirect } from "next/navigation"
-import { createBooking } from "@/lib/sendBooking"
+'use server';
+import { createMessage } from '@/lib/sendMessage';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { createBooking } from '@/lib/sendBooking';
 
-import {createRoomImage} from "@/lib/sendRoomImg"
+import { createRoomImage } from '@/lib/sendRoomImg';
 
-import {uploadImage} from "@/lib/cloudinary"
-import { createNightImage } from "@/lib/sendNightImg"
-import { createDiningImage } from "@/lib/sendDiningImg"
-import { createGardenImage } from "@/lib/sendGardenImg"
+import { uploadImage } from '@/lib/cloudinary';
+import { createNightImage } from '@/lib/sendNightImg';
+import { createDiningImage } from '@/lib/sendDiningImg';
+import { createGardenImage } from '@/lib/sendGardenImg';
 
-<<<<<<< HEAD
-export default async function contactSubmission(prevState,formData){
-=======
-export default async function ContactSubmission(prevState,formData){
->>>>>>> efc30f0731400b2545a06a6a60480a0c855c7609
-    
-    const firstName =formData.get('firstN')
-    const secondName = formData.get('secondN')
-    const emailAddress = formData.get('email')
-    const contactNumber = formData.get('Number')
-    const description = formData.get('description')
-<<<<<<< HEAD
-    console.log(firstName,secondName,emailAddress,contactNumber,description)
-=======
+export default async function ContactSubmission(prevState, formData) {
+  // >>>>>>> efc30f0731400b2545a06a6a60480a0c855c7609
 
->>>>>>> efc30f0731400b2545a06a6a60480a0c855c7609
+  const firstName = formData.get('firstN');
+  const secondName = formData.get('secondN');
+  const emailAddress = formData.get('email');
+  const contactNumber = formData.get('Number');
+  const description = formData.get('description');
+  // <<<<<<< HEAD
+  console.log(firstName, secondName, emailAddress, contactNumber, description);
+  // =======
 
-   
-    let errors = [];
+  // >>>>>>> efc30f0731400b2545a06a6a60480a0c855c7609
 
-    if (!firstName || firstName.trim().length === 0) {
-      errors.push('first Name is required.');
-    }
-  
-    if (!secondName || secondName.trim().length === 0) {
-      errors.push('Last name is required.');
-    }
-    if (!contactNumber || contactNumber.trim().length === 0) {
-      errors.push('Contact Number is required.');
-    }
-    if (!emailAddress || emailAddress.trim().length === 0) {
-      errors.push('email address is required.');
-    }
-    if (!description || description.trim().length === 0) {
-      errors.push('Message is required is required.');
-    }
-  
-    
-  
-    if (errors.length > 0) {
-      return { errors };}
+  let errors = [];
 
-
-    await createMessage({
-        firstName,
-        secondName,
-        emailAddress,
-        contactNumber,
-        description,
-        User_id: 1
-
-    })
-    revalidatePath('/', 'layout')
-  redirect('/');
-    
+  if (!firstName || firstName.trim().length === 0) {
+    errors.push('first Name is required.');
   }
-  
 
+  if (!secondName || secondName.trim().length === 0) {
+    errors.push('Last name is required.');
+  }
+  if (!contactNumber || contactNumber.trim().length === 0) {
+    errors.push('Contact Number is required.');
+  }
+  if (!emailAddress || emailAddress.trim().length === 0) {
+    errors.push('email address is required.');
+  }
+  if (!description || description.trim().length === 0) {
+    errors.push('Message is required is required.');
+  }
 
-export async function BookingSubmission (prevState,formData){
-  const firstName =formData.get('firstName')
-    const secondName = formData.get('lastName')
-    const emailAddress = formData.get('emailAdd')
-    const contactNumber = formData.get('Number')
-    const description = formData.get('description')
-    const guests = formData.get('guests')
-    const roomtype = formData.get("roomtype") 
+  if (errors.length > 0) {
+    return { errors };
+  }
 
-<<<<<<< HEAD
-    console.log(firstName,secondName,emailAddress,contactNumber,description,guests,roomtype)
-=======
-    
->>>>>>> efc30f0731400b2545a06a6a60480a0c855c7609
-
-
-    await createBooking({
-      firstName,
-      secondName,
-      emailAddress,
-      contactNumber,
-      description,
-      guests,
-      roomtype,
-      user_id: 1
-    })
-
-    revalidatePath('/', 'layout')
-    redirect('/');
-  
+  await createMessage({
+    firstName,
+    secondName,
+    emailAddress,
+    contactNumber,
+    description,
+    User_id: 1,
+  });
+  revalidatePath('/', 'layout');
+  redirect('/');
 }
-<<<<<<< HEAD
-export async function gardenImageSubmission(prevState,formData){
-=======
-export async function GardenImageSubmission(prevState,formData){
->>>>>>> efc30f0731400b2545a06a6a60480a0c855c7609
-  const Image = formData.get("ImageGarden")
+
+export async function BookingSubmission(prevState, formData) {
+  const firstName = formData.get('firstName');
+  const secondName = formData.get('lastName');
+  const emailAddress = formData.get('emailAdd');
+  const contactNumber = formData.get('Number');
+  const description = formData.get('description');
+  const guests = formData.get('guests');
+  const roomtype = formData.get('roomtype');
+
+  // <<<<<<< HEAD
+  console.log(
+    firstName,
+    secondName,
+    emailAddress,
+    contactNumber,
+    description,
+    guests,
+    roomtype
+  );
+  // =======
+
+  // >>>>>>> efc30f0731400b2545a06a6a60480a0c855c7609
+
+  await createBooking({
+    firstName,
+    secondName,
+    emailAddress,
+    contactNumber,
+    description,
+    guests,
+    roomtype,
+    user_id: 1,
+  });
+
+  revalidatePath('/', 'layout');
+  redirect('/');
+}
+// <<<<<<< HEAD
+export async function gardenImageSubmission(prevState, formData) {
+  const Image = formData.get('ImageGarden');
 
   let errors = [];
   if (!Image || Image.size === 0) {
@@ -127,14 +120,12 @@ export async function GardenImageSubmission(prevState,formData){
   }
   await createGardenImage({
     imageUrl: imageUrl,
-  })
-  revalidatePath('/', 'layout')
+  });
+  revalidatePath('/', 'layout');
   redirect('/admin/dashboard/garden');
-  
-  
 }
-export async function roomImageSubmission(prevState,formData){
-  const Image = formData.get("ImageRoom")
+export async function roomImageSubmission(prevState, formData) {
+  const Image = formData.get('ImageRoom');
   let errors = [];
   if (!Image || Image.size === 0) {
     errors.push('Image is required.');
@@ -154,17 +145,13 @@ export async function roomImageSubmission(prevState,formData){
   }
   await createRoomImage({
     imageUrl: imageUrl,
-  })
-  revalidatePath('/', 'layout')
+  });
+  revalidatePath('/', 'layout');
   redirect('/admin/dashboard/room');
-
 }
-<<<<<<< HEAD
-export async function diningImageSubmission(prevState,formData){
-=======
-export async function DiningImageSubmission(prevState,formData){
->>>>>>> efc30f0731400b2545a06a6a60480a0c855c7609
-  const Image = formData.get("ImageDining")
+export async function DiningImageSubmission(prevState, formData) {
+  // >>>>>>> efc30f0731400b2545a06a6a60480a0c855c7609
+  const Image = formData.get('ImageDining');
   let errors = [];
   if (!Image || Image.size === 0) {
     errors.push('Image is required.');
@@ -182,20 +169,15 @@ export async function DiningImageSubmission(prevState,formData){
       'Image upload failed, post was not created. Please try again later.'
     );
   }
-   await createDiningImage({
+  await createDiningImage({
     imageUrl: imageUrl,
-  }) 
-  revalidatePath('/', 'layout')
-  redirect('/admin/dashboard/dining')
-
-  
+  });
+  revalidatePath('/', 'layout');
+  redirect('/admin/dashboard/dining');
 }
 
-
-
-
-export async function nightImageSubmission(prevState,formData){
-  const Image = formData.get("ImageNight")
+export async function nightImageSubmission(prevState, formData) {
+  const Image = formData.get('ImageNight');
   let errors = [];
   if (!Image || Image.size === 0) {
     errors.push('Image is required.');
@@ -213,12 +195,10 @@ export async function nightImageSubmission(prevState,formData){
       'Image upload failed, post was not created. Please try again later.'
     );
   }
-  
+
   await createNightImage({
     imageUrl: imageUrl,
-    
   });
-  revalidatePath('/', 'layout')
-  redirect('/admin/dashboard/night')
-
+  revalidatePath('/', 'layout');
+  redirect('/admin/dashboard/night');
 }
